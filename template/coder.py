3b12844eab7ae142f68ae8045c7c2a1f282382cd
yukirin <standupdown@gmail.com>
2019-06-07 18:49:52 +0900

    Refactoring

--- template/coder.py
+++ template/coder.py
@@ -1,9 +1,6 @@
-# import numpy as np
-# from scipy import linalg
-from sys import stdin
 import sys
 import os
-
+from sys import stdin
 
 stdin = open(os.path.dirname(__file__) + '/input.txt')  # local test
 input = stdin.readline
