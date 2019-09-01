6fa83fb75c82efd22536e1b4acb466b25f5ee5fc
yukirin <standupdown@gmail.com>
2019-09-01 22:55:06 +0900

    Add  Typical DP C

--- atcoder/tdpc/C/main.cpp
+++ atcoder/tdpc/C/main.cpp
@@ -9,7 +9,7 @@
 #define all(s) begin(s), end(s)
 #define rall(s) rbegin(s), rend(s)
 #define rep(i, a, b) for (int i = (a); i < (b); i++)
-#define rrep(i, a, b) for (int i = ((a) - 1); i >= (b); i--)
+#define rrep(i, a, b) for (int i = ((a)-1); i >= (b); i--)
 #define pb push_back
 #define sz(a) int((a).size())
 
@@ -36,8 +36,28 @@ template <class T> static inline bool chmax(T& a, T b);
 template <class T> static inline bool chmin(T& a, T b);
 template <class A, size_t N, class T> static void Fill(A (&arr)[N], const T& val);
 
-int main(int argc, char* argv[]) {
+double win_rate(double rp, double rq) { return 1 / (1 + pow(10, (rq - rp) / 400)); }
 
+double dp[13][1100];
+
+int main(int argc, char* argv[]) {
+  int k;
+  cin >> k;
+  ll n = static_cast<ll>(pow(2, k));
+  vector<int> ratings(n);
+  scan(ratings);
+  rep(i, 0, n) dp[0][i] = 1.0;
+
+  rep(i, 0, k) {
+    rep(j, 0, n) {
+      int mask = 0xffffffff << i;
+      int index = (j & mask) ^ (1 << i);
+      rep(l, 0, 1 << i) { dp[i + 1][j] += win_rate(ratings[j], ratings[index + l]) * dp[i][index + l]; }
+      dp[i + 1][j] *= dp[i][j];
+    }
+  }
+
+  rep(i, 0, n) { cout << fixed << setprecision(9) << dp[k][i] << endl; }
   return 0;
 }
 
