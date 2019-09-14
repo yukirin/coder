7eabd20fe9862e86a32d25ceb9f41fe6a7d49826
yukirin <standupdown@gmail.com>
2019-09-15 03:34:16 +0900

    Add ABC133

---
+++ atcoder/abc133/B/main.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+
+#ifdef _DEBUG
+#define debug(x) cerr << "line: " << __LINE__ << ", func: " << __func__ << " ->  " << #x << " = " << x << endl
+#else
+#define debug(x)
+#endif
+
+#define all(s) begin(s), end(s)
+#define rall(s) rbegin(s), rend(s)
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = ((a)-1); i >= (b); i--)
+#define pb push_back
+#define sz(a) int((a).size())
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using i_i = pair<int, int>;
+using ll_ll = pair<ll, ll>;
+using d_ll = pair<double, ll>;
+using ll_d = pair<ll, double>;
+using d_d = pair<double, double>;
+
+static constexpr ll LL_INF = 1LL << 60;
+static constexpr int I_INF = 1 << 28;
+static constexpr double PI = static_cast<double>(3.14159265358979323846264338327950288);
+static constexpr double EPS = numeric_limits<double>::epsilon();
+
+static map<type_index, const char* const> scanType = {
+    {typeid(int), "%d"}, {typeid(ll), "%lld"}, {typeid(double), "%lf"}, {typeid(char), "%c"}};
+
+template <class T> static void scan(vector<T>& v);
+[[maybe_unused]] static void scan(vector<string>& v, bool isWord = true);
+template <class T> static inline bool chmax(T& a, T b);
+template <class T> static inline bool chmin(T& a, T b);
+template <class A, size_t N, class T> static void Fill(A (&arr)[N], const T& val);
+
+bool dist(vector<ll>& a, vector<ll>& b) {
+  int l = sz(a);
+  double d = 0.0;
+  rep(i, 0, l) { d += pow(a[i] - b[i], 2); };
+  d = sqrt(d);
+
+  return abs(ceil(d) - d) <= EPS;
+}
+
+int main(int argc, char* argv[]) {
+  long long N;
+  scanf("%lld", &N);
+  long long D;
+  scanf("%lld", &D);
+  std::vector<std::vector<long long>> X(N, std::vector<long long>(D));
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < D; j++) {
+      scanf("%lld", &X[i][j]);
+    }
+  }
+
+  ll ans = 0;
+  rep(i, 0, N) rep(j, i + 1, N) { ans += dist(X[i], X[j]); }
+
+  cout << ans << endl;
+  return 0;
+}
+
+template <class T> static void scan(vector<T>& v) {
+  auto tFormat = scanType[typeid(T)];
+  for (T& n : v) {
+    scanf(tFormat, &n);
+  }
+}
+
+static void scan(vector<string>& v, bool isWord) {
+  if (isWord) {
+    for (auto& n : v) {
+      cin >> n;
+    }
+    return;
+  }
+
+  int i = 0, size = v.size();
+  string s;
+  getline(cin, s);
+
+  if (s.size() != 0) {
+    i++;
+    v[0] = s;
+  }
+
+  for (; i < size; ++i) {
+    getline(cin, v[i]);
+  }
+}
+
+template <class T> inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <class T> inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <class A, size_t N, class T> void Fill(A (&arr)[N], const T& val) {
+  std::fill((T*)arr, (T*)(arr + N), val);
+}
