ff5b0e5ae9af9a35e97d1d98499863a13e77f069
yukirin <standupdown@gmail.com>
2019-10-19 21:16:14 +0900

    Add ABC115

---
+++ atcoder/abc115/D/main.cpp
@@ -0,0 +1,143 @@
+#include <bits/stdc++.h>
+
+#ifdef _DEBUG
+#define debug(x) cerr << "line: " << __LINE__ << ", func: " << __func__ << " ->  " << #x << " = " << x << endl
+#else
+#define debug(x)
+#endif
+
+#define all(s) begin(s), end(s)
+#define rall(s) rbegin(s), rend(s)
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = ((a)-1); i >= (b); i--)
+#define pb push_back
+#define sz(a) int((a).size())
+#define put(a) ((cout) << (a) << (endl))
+#define putf(a, n) ((cout) << (fixed) << (setprecision(n)) << (a) << (endl))
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using i_i = pair<int, int>;
+using ll_ll = pair<ll, ll>;
+using d_ll = pair<double, ll>;
+using ll_d = pair<ll, double>;
+using d_d = pair<double, double>;
+
+static constexpr ll LL_INF = 1LL << 60;
+static constexpr int I_INF = 1 << 28;
+static constexpr double PI = static_cast<double>(3.14159265358979323846264338327950288);
+static constexpr double EPS = numeric_limits<double>::epsilon();
+
+static map<type_index, const char* const> scanType = {
+    {typeid(int), "%d"}, {typeid(ll), "%lld"}, {typeid(double), "%lf"}, {typeid(char), "%c"}};
+
+template <class T> static void scan(vector<T>& v);
+[[maybe_unused]] static void scan(vector<string>& v, bool isWord = true);
+template <class T> static inline bool chmax(T& a, T b);
+template <class T> static inline bool chmin(T& a, T b);
+template <class T> static inline T gcd(T a, T b);
+template <class T> static inline T lcm(T a, T b);
+template <class A, size_t N, class T> static void Fill(A (&arr)[N], const T& val);
+
+ll_ll memo[55];
+
+ll_ll dfs(int n) {
+  if (n == 0) return memo[0] = {1, 1};
+  if (memo[n] != ll_ll{0, 0}) return memo[n];
+
+  ll_ll n_minus = dfs(n - 1);
+  ll_ll count{1, 0};
+  count.first += (n_minus.first + 1 + n_minus.first + 1);
+  count.second += (n_minus.second + 1 + n_minus.second);
+
+  return memo[n] = count;
+}
+
+int main(int argc, char* argv[]) {
+  ll n, x;
+  cin >> n >> x;
+
+  dfs(n);
+  ll ans = 0;
+  while (x != 0) {
+    ll_ll count = memo[n];
+    if (x == count.first) {
+      ans += count.second;
+      break;
+    }
+
+    ll_ll n_minus = memo[n - 1];
+    if (x == (n_minus.first + 2)) {
+      ans += (n_minus.second + 1);
+      break;
+    }
+
+    if (x > (n_minus.first + 2)) {
+      x -= (n_minus.first + 2);
+      ans += (n_minus.second + 1);
+      n--;
+      continue;
+    }
+
+    x--;
+    n--;
+  }
+
+  put(ans);
+  return 0;
+}
+
+template <class T> static void scan(vector<T>& v) {
+  auto tFormat = scanType[typeid(T)];
+  for (T& n : v) {
+    scanf(tFormat, &n);
+  }
+}
+
+static void scan(vector<string>& v, bool isWord) {
+  if (isWord) {
+    for (auto& n : v) {
+      cin >> n;
+    }
+    return;
+  }
+
+  int i = 0, size = v.size();
+  string s;
+  getline(cin, s);
+
+  if (s.size() != 0) {
+    i++;
+    v[0] = s;
+  }
+
+  for (; i < size; ++i) {
+    getline(cin, v[i]);
+  }
+}
+
+template <class T> inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <class T> inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <class T> inline T gcd(T a, T b) { return __gcd(a, b); }
+
+template <class T> inline T lcm(T a, T b) { return (a * b) / gcd(a, b); }
+
+template <class A, size_t N, class T> void Fill(A (&arr)[N], const T& val) {
+  std::fill((T*)arr, (T*)(arr + N), val);
+}
