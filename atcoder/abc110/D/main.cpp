0e2c9b54e85bb5731b5375cd1625fdc577afbef8
yukirin <standupdown@gmail.com>
2019-11-03 21:12:34 +0900

    Add ABC110

---
+++ atcoder/abc110/D/main.cpp
@@ -0,0 +1,154 @@
+#include <bits/stdc++.h>
+
+#ifdef _DEBUG
+#define debug(x) cerr << "line: " << __LINE__ << ", func: " << __func__ << " ->  " << #x << " = " << x << endl
+#else
+#define debug(x)
+#endif
+
+#define all(s) begin(s), end(s)
+#define rall(s) rbegin(s), rend(s)
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = ((a)-1); i >= (b); i--)
+#define pb push_back
+#define sz(a) int((a).size())
+#define put(a) ((cout) << (a) << (endl))
+#define putf(a, n) ((cout) << (fixed) << (setprecision(n)) << (a) << (endl))
+#define deg2rad(x) (((x)*PI) / (180.0))
+#define rad2deg(x) (((x) * (180.0)) / PI)
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using i_i = pair<int, int>;
+using ll_ll = pair<ll, ll>;
+using d_ll = pair<double, ll>;
+using ll_d = pair<ll, double>;
+using d_d = pair<double, double>;
+
+static constexpr ll LL_INF = 1LL << 60;
+static constexpr int I_INF = 1 << 28;
+static constexpr double PI = static_cast<double>(3.14159265358979323846264338327950288);
+static constexpr double EPS = numeric_limits<double>::epsilon();
+static constexpr ll MOD = 1000000007;
+
+static map<type_index, const char* const> scanType = {
+    {typeid(int), "%d"}, {typeid(ll), "%lld"}, {typeid(double), "%lf"}, {typeid(char), "%c"}};
+
+template <class T> static void scan(vector<T>& v);
+[[maybe_unused]] static void scan(vector<string>& v, bool isWord = true);
+template <class T> static inline bool chmax(T& a, T b);
+template <class T> static inline bool chmin(T& a, T b);
+template <class T> static inline T gcd(T a, T b);
+template <class T> static inline T lcm(T a, T b);
+template <class A, size_t N, class T> static void Fill(A (&arr)[N], const T& val);
+template <class T> T mod(T a, T m);
+
+vector<ll_ll> prime_factor(ll n) {
+  vector<ll_ll> res;
+  for (ll i = 2; i * i <= n; i++) {
+    if (n % i) continue;
+    res.emplace_back(i, 0);
+    while (n % i == 0) {
+      n /= i;
+      res.back().second++;
+    }
+  }
+
+  if (n != 1) res.emplace_back(n, 1);
+  return res;
+}
+
+static constexpr int MAX = 510000;
+ll fac[MAX], finv[MAX], inv[MAX];
+
+// 前処理
+void comb_init() {
+  fac[0] = fac[1] = 1;
+  finv[0] = finv[1] = 1;
+  inv[1] = 1;
+  for (int i = 2; i < MAX; i++) {
+    fac[i] = fac[i - 1] * i % MOD;
+    inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+    finv[i] = finv[i - 1] * inv[i] % MOD;
+  }
+}
+
+// nCr mod p
+ll comb(int n, int k) {
+  if (n < k) return 0;
+  if (n < 0 || k < 0) return 0;
+  return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+int main(int argc, char* argv[]) {
+  ll n, m;
+  cin >> n >> m;
+
+  comb_init();
+  auto ps = prime_factor(m);
+
+  ll ans = 1;
+  for (auto p : ps) {
+    ans = mod(ans * comb(p.second + n - 1, n - 1), MOD);
+  }
+
+  put(ans);
+  return 0;
+}
+
+template <class T> static void scan(vector<T>& v) {
+  auto tFormat = scanType[typeid(T)];
+  for (T& n : v) {
+    scanf(tFormat, &n);
+  }
+}
+
+static void scan(vector<string>& v, bool isWord) {
+  if (isWord) {
+    for (auto& n : v) {
+      cin >> n;
+    }
+    return;
+  }
+
+  int i = 0, size = v.size();
+  string s;
+  getline(cin, s);
+
+  if (s.size() != 0) {
+    i++;
+    v[0] = s;
+  }
+
+  for (; i < size; ++i) {
+    getline(cin, v[i]);
+  }
+}
+
+template <class T> inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <class T> inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <class T> inline T gcd(T a, T b) { return __gcd(a, b); }
+
+template <class T> inline T lcm(T a, T b) { return (a * b) / gcd(a, b); }
+
+template <class A, size_t N, class T> void Fill(A (&arr)[N], const T& val) {
+  std::fill((T*)arr, (T*)(arr + N), val);
+}
+
+template <class T> T mod(T a, T m) { return (a % m + m) % m; }
