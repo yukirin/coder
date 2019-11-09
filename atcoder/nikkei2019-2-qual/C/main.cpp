e0cca1581b78f83a34e42188599c2c96d50bd761
yukirin <standupdown@gmail.com>
2019-11-10 00:21:27 +0900

    Add Nikkei2019-2

---
+++ atcoder/nikkei2019-2-qual/C/main.cpp
@@ -0,0 +1,141 @@
+#include <bits/stdc++.h>
+
+#ifdef _DEBUG
+#define debug(x) cerr << "line: " << __LINE__ << ", func: " << __func__ << " ->  " << #x << " = " << x << endl
+#else
+#define debug(x)
+#endif
+
+#define all(s) begin(s), end(s)
+#define rall(s) rbegin(s), rend(s)
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = ((a)-1); i >= (b); i--)
+#define pb push_back
+#define sz(a) int((a).size())
+#define put(a) ((cout) << (a) << (endl))
+#define putf(a, n) ((cout) << (fixed) << (setprecision(n)) << (a) << (endl))
+#define deg2rad(x) (((x)*PI) / (180.0))
+#define rad2deg(x) (((x) * (180.0)) / PI)
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using i_i = pair<int, int>;
+using ll_ll = pair<ll, ll>;
+using d_ll = pair<double, ll>;
+using ll_d = pair<ll, double>;
+using d_d = pair<double, double>;
+
+static constexpr ll LL_INF = 1LL << 60;
+static constexpr int I_INF = 1 << 28;
+static constexpr double PI = static_cast<double>(3.14159265358979323846264338327950288);
+static constexpr double EPS = numeric_limits<double>::epsilon();
+static constexpr const char* YES = "Yes";
+static constexpr const char* NO = "No";
+
+static map<type_index, const char* const> scanType = {
+    {typeid(int), "%d"}, {typeid(ll), "%lld"}, {typeid(double), "%lf"}, {typeid(char), "%c"}};
+
+template <class T> static void scan(vector<T>& v);
+[[maybe_unused]] static void scan(vector<string>& v, bool isWord = true);
+template <class T> static inline bool chmax(T& a, T b);
+template <class T> static inline bool chmin(T& a, T b);
+template <class T> static inline T gcd(T a, T b);
+template <class T> static inline T lcm(T a, T b);
+template <class A, size_t N, class T> static void Fill(A (&arr)[N], const T& val);
+template <class T> T mod(T a, T m);
+
+int main(int argc, char* argv[]) {
+  long long N;
+  scanf("%lld", &N);
+  std::vector<long long> A(N), SA(N), NA(N);
+  vector<ll_ll> v(N);
+  for (int i = 0; i < N; i++) {
+    scanf("%lld", &A[i]);
+    SA[i] = A[i];
+  }
+  std::vector<long long> B(N), SB(N);
+  for (int i = 0; i < N; i++) {
+    scanf("%lld", &B[i]);
+    SB[i] = B[i];
+    v[i].first = B[i];
+    v[i].second = i;
+  }
+
+  sort(all(SA));
+  sort(all(SB));
+  sort(all(v));
+  rep(i, 0, N) {
+    if (SA[i] > SB[i]) {
+      put("No");
+      return 0;
+    }
+  }
+
+  rep(i, 0, N) { NA[i] = A[v[i].second]; }
+
+  rep(i, 0, N) {
+    if (NA[i] <= SB[i]) {
+      put("Yes");
+      return 0;
+    }
+  }
+  put("No");
+  return 0;
+}
+
+template <class T> static void scan(vector<T>& v) {
+  auto tFormat = scanType[typeid(T)];
+  for (T& n : v) {
+    scanf(tFormat, &n);
+  }
+}
+
+static void scan(vector<string>& v, bool isWord) {
+  if (isWord) {
+    for (auto& n : v) {
+      cin >> n;
+    }
+    return;
+  }
+
+  int i = 0, size = v.size();
+  string s;
+  getline(cin, s);
+
+  if (s.size() != 0) {
+    i++;
+    v[0] = s;
+  }
+
+  for (; i < size; ++i) {
+    getline(cin, v[i]);
+  }
+}
+
+template <class T> inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <class T> inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <class T> inline T gcd(T a, T b) { return __gcd(a, b); }
+
+template <class T> inline T lcm(T a, T b) { return (a * b) / gcd(a, b); }
+
+template <class A, size_t N, class T> void Fill(A (&arr)[N], const T& val) {
+  std::fill((T*)arr, (T*)(arr + N), val);
+}
+
+template <class T> T mod(T a, T m) { return (a % m + m) % m; }
