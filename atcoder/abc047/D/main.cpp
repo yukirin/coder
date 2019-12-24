914c71f984157e0a37ff6d802f6cf9063888d6c2
yukirin <standupdown@gmail.com>
2019-12-24 15:28:02 +0900

    Add abc047

---
+++ atcoder/abc047/D/main.cpp
@@ -0,0 +1,221 @@
+#include <bits/stdc++.h>
+
+#ifdef _DEBUG
+#define debug(x) cerr << "line: " << __LINE__ << ", func: " << __func__ << " ->  " << #x << " = " << x << endl
+#else
+#define debug(x)
+#endif
+
+#define all(s) begin(s), end(s)
+#define rall(s) rbegin(s), rend(s)
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = ((a)-1); i >= (b); i--)
+#define pb push_back
+#define sz(a) int((a).size())
+#define put(a) ((cout) << (a) << (endl))
+#define putf(a, n) ((cout) << (fixed) << (setprecision(n)) << (a) << (endl))
+#define deg2rad(x) (((x)*PI) / (180.0))
+#define rad2deg(x) (((x) * (180.0)) / PI)
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using i_i = pair<int, int>;
+using ll_ll = pair<ll, ll>;
+using d_ll = pair<double, ll>;
+using ll_d = pair<ll, double>;
+using d_d = pair<double, double>;
+template <class T> using vec = vector<T>;
+
+static constexpr ll LL_INF = 1LL << 60;
+static constexpr int I_INF = 1 << 28;
+static constexpr double PI = static_cast<double>(3.14159265358979323846264338327950288);
+static constexpr double EPS = numeric_limits<double>::epsilon();
+
+static map<type_index, const char* const> scanType = {
+    {typeid(int), "%d"}, {typeid(ll), "%lld"}, {typeid(double), "%lf"}, {typeid(char), "%c"}};
+
+template <class T> static void scan(vector<T>& v);
+[[maybe_unused]] static void scan(vector<string>& v, bool isWord = true);
+template <class T> static inline bool chmax(T& a, T b);
+template <class T> static inline bool chmin(T& a, T b);
+template <class T> static inline T gcd(T a, T b);
+template <class T> static inline T lcm(T a, T b);
+template <class A, size_t N, class T> static void Fill(A (&arr)[N], const T& val);
+template <class T> T mod(T a, T m);
+
+template <class Monoid> struct SegmentTree {
+  using F = function<Monoid(Monoid, Monoid)>;
+
+  int sz;
+  vector<Monoid> seg;
+
+  const F f;
+  const Monoid M1;
+
+  SegmentTree(int n, const F f, const Monoid& M1) : f(f), M1(M1) {
+    sz = 1;
+    while (sz < n) sz <<= 1;
+    seg.assign(2 * sz, M1);
+  }
+
+  void set(int k, const Monoid& x) { seg[k + sz] = x; }
+
+  void build() {
+    for (int k = sz - 1; k > 0; k--) {
+      seg[k] = f(seg[2 * k + 0], seg[2 * k + 1]);
+    }
+  }
+
+  void update(int k, const Monoid& x) {
+    k += sz;
+    seg[k] = x;
+    while (k >>= 1) {
+      seg[k] = f(seg[2 * k + 0], seg[2 * k + 1]);
+    }
+  }
+
+  Monoid query(int a, int b) {
+    Monoid L = M1, R = M1;
+    for (a += sz, b += sz; a < b; a >>= 1, b >>= 1) {
+      if (a & 1) L = f(L, seg[a++]);
+      if (b & 1) R = f(seg[--b], R);
+    }
+    return f(L, R);
+  }
+
+  Monoid operator[](const int& k) const { return seg[k + sz]; }
+
+  template <class C> int find_subtree(int a, const C& check, Monoid& M, bool type) {
+    while (a < sz) {
+      Monoid nxt = type ? f(seg[2 * a + type], M) : f(M, seg[2 * a + type]);
+      if (check(nxt))
+        a = 2 * a + type;
+      else
+        M = nxt, a = 2 * a + 1 - type;
+    }
+    return a - sz;
+  }
+
+  template <class C> int find_first(int a, const C& check) {
+    Monoid L = M1;
+    if (a <= 0) {
+      if (check(f(L, seg[1]))) return find_subtree(1, check, L, false);
+      return -1;
+    }
+    int b = sz;
+    for (a += sz, b += sz; a < b; a >>= 1, b >>= 1) {
+      if (a & 1) {
+        Monoid nxt = f(L, seg[a]);
+        if (check(nxt)) return find_subtree(a, check, L, false);
+        L = nxt;
+        ++a;
+      }
+    }
+    return -1;
+  }
+
+  template <class C> int find_last(int b, const C& check) {
+    Monoid R = M1;
+    if (b >= sz) {
+      if (check(f(seg[1], R))) return find_subtree(1, check, R, true);
+      return -1;
+    }
+    int a = sz;
+    for (b += sz; a < b; a >>= 1, b >>= 1) {
+      if (b & 1) {
+        Monoid nxt = f(seg[--b], R);
+        if (check(nxt)) return find_subtree(b, check, R, true);
+        R = nxt;
+      }
+    }
+    return -1;
+  }
+};
+
+int main(int argc, char* argv[]) {
+  ll n, t;
+  cin >> n >> t;
+  vec<ll> a(n);
+  scan(a);
+
+  auto f = [](ll x, ll y) { return max(x, y); };
+  SegmentTree<ll> seg(n, f, -LL_INF);
+  rep(i, 0, n) seg.set(i, a[i]);
+  seg.build();
+
+  ll max_diff = 0, count = 0;
+  rep(i, 0, n - 1) {
+    ll diff = seg.query(i + 1, n) - a[i];
+    if (diff < max_diff) continue;
+    if (diff == max_diff) {
+      count++;
+      continue;
+    }
+    max_diff = diff;
+    count = 1;
+  }
+
+  put(count);
+  return 0;
+}
+
+template <class T> static void scan(vector<T>& v) {
+  auto tFormat = scanType[typeid(T)];
+  for (T& n : v) {
+    scanf(tFormat, &n);
+  }
+}
+
+static void scan(vector<string>& v, bool isWord) {
+  if (isWord) {
+    for (auto& n : v) {
+      cin >> n;
+    }
+    return;
+  }
+
+  int i = 0, size = v.size();
+  string s;
+  getline(cin, s);
+
+  if (s.size() != 0) {
+    i++;
+    v[0] = s;
+  }
+
+  for (; i < size; ++i) {
+    getline(cin, v[i]);
+  }
+}
+
+template <class T> inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <class T> inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <class T> inline T gcd(T a, T b) { return __gcd(a, b); }
+
+template <class T> inline T lcm(T a, T b) {
+  T c = min(a, b), d = max(a, b);
+  return c * (d / gcd(c, d));
+}
+
+template <class A, size_t N, class T> void Fill(A (&arr)[N], const T& val) {
+  std::fill((T*)arr, (T*)(arr + N), val);
+}
+
+template <class T> T mod(T a, T m) { return (a % m + m) % m; }
