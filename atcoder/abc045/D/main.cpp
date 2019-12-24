abab2b6fb49489ad0eb116736b0b8807bed2c6d4
yukirin <standupdown@gmail.com>
2019-12-24 20:39:07 +0900

    Add abc045

---
+++ atcoder/abc045/D/main.cpp
@@ -0,0 +1,144 @@
+#include <bits/stdc++.h>
+
+#ifdef _DEBUG
+#define debug(x) cerr << "line: " << __LINE__ << ", func: " << __func__ << " ->  " << #x << " = " << x << endl
+#else
+#define debug(x)
+#endif
+
+#define all(s) begin(s), end(s)
+#define rall(s) rbegin(s), rend(s)
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = ((a)-1); i >= (b); i--)
+#define pb push_back
+#define sz(a) int((a).size())
+#define put(a) ((cout) << (a) << (endl))
+#define putf(a, n) ((cout) << (fixed) << (setprecision(n)) << (a) << (endl))
+#define deg2rad(x) (((x)*PI) / (180.0))
+#define rad2deg(x) (((x) * (180.0)) / PI)
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using i_i = pair<int, int>;
+using ll_ll = pair<ll, ll>;
+using d_ll = pair<double, ll>;
+using ll_d = pair<ll, double>;
+using d_d = pair<double, double>;
+template <class T> using vec = vector<T>;
+
+static constexpr ll LL_INF = 1LL << 60;
+static constexpr int I_INF = 1 << 28;
+static constexpr double PI = static_cast<double>(3.14159265358979323846264338327950288);
+static constexpr double EPS = numeric_limits<double>::epsilon();
+
+static map<type_index, const char* const> scanType = {
+    {typeid(int), "%d"}, {typeid(ll), "%lld"}, {typeid(double), "%lf"}, {typeid(char), "%c"}};
+
+template <class T> static void scan(vector<T>& v);
+[[maybe_unused]] static void scan(vector<string>& v, bool isWord = true);
+template <class T> static inline bool chmax(T& a, T b);
+template <class T> static inline bool chmin(T& a, T b);
+template <class T> static inline T gcd(T a, T b);
+template <class T> static inline T lcm(T a, T b);
+template <class A, size_t N, class T> static void Fill(A (&arr)[N], const T& val);
+template <class T> T mod(T a, T m);
+
+int main(int argc, char* argv[]) {
+  ll h, w, n;
+  cin >> h >> w >> n;
+  vec<int> a(n), b(n);
+
+  map<i_i, bool> black, visited;
+  for (int i = 0; i < n; i++) {
+    scanf("%d %d", &a[i], &b[i]);
+    a[i]--, b[i]--;
+    black[make_pair(a[i], b[i])] = true;
+  }
+
+  vec<ll> ans(10);
+  rep(i, 0, n) {
+    i_i pivot{a[i], b[i]};
+    rep(c_h, -2, 1) rep(c_w, -2, 1) {
+      i_i corner{pivot.first + c_h, pivot.second + c_w};
+      ll count = 0;
+
+      if (visited[corner]) continue;
+      if (corner.first < 0 || corner.second < 0) continue;
+      if (corner.first > h - 3 || corner.second > w - 3) continue;
+      visited[corner] = true;
+
+      rep(offset_x, 0, 3) rep(offset_y, 0, 3) {
+        i_i pos{corner.first + offset_x, corner.second + offset_y};
+        if (black[pos]) count++;
+      }
+
+      ans[count]++;
+    }
+  }
+  ans[0] = 0;
+  ans[0] = (h - 2) * (w - 2) - accumulate(all(ans), 0LL);
+
+  rep(i, 0, 10) put(ans[i]);
+  return 0;
+}
+
+template <class T> static void scan(vector<T>& v) {
+  auto tFormat = scanType[typeid(T)];
+  for (T& n : v) {
+    scanf(tFormat, &n);
+  }
+}
+
+static void scan(vector<string>& v, bool isWord) {
+  if (isWord) {
+    for (auto& n : v) {
+      cin >> n;
+    }
+    return;
+  }
+
+  int i = 0, size = v.size();
+  string s;
+  getline(cin, s);
+
+  if (s.size() != 0) {
+    i++;
+    v[0] = s;
+  }
+
+  for (; i < size; ++i) {
+    getline(cin, v[i]);
+  }
+}
+
+template <class T> inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <class T> inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <class T> inline T gcd(T a, T b) { return __gcd(a, b); }
+
+template <class T> inline T lcm(T a, T b) {
+  T c = min(a, b), d = max(a, b);
+  return c * (d / gcd(c, d));
+}
+
+template <class A, size_t N, class T> void Fill(A (&arr)[N], const T& val) {
+  std::fill((T*)arr, (T*)(arr + N), val);
+}
+
+template <class T> T mod(T a, T m) { return (a % m + m) % m; }
