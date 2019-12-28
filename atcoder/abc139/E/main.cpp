d7a647b92a70fe3d819f102e6c7ac13517a92810
yukirin <standupdown@gmail.com>
2019-12-29 00:49:53 +0900

    Update abc139: E

--- atcoder/abc139/E/main.cpp
+++ atcoder/abc139/E/main.cpp
@@ -12,6 +12,8 @@
 #define rrep(i, a, b) for (int i = ((a)-1); i >= (b); i--)
 #define pb push_back
 #define sz(a) int((a).size())
+#define fi first
+#define se second
 
 using namespace std;
 
@@ -22,6 +24,7 @@ using ll_ll = pair<ll, ll>;
 using d_ll = pair<double, ll>;
 using ll_d = pair<ll, double>;
 using d_d = pair<double, double>;
+template <class T> using vec = vector<T>;
 
 static constexpr ll LL_INF = 1LL << 60;
 static constexpr int I_INF = 1 << 28;
@@ -37,49 +40,81 @@ template <class T> static inline bool chmax(T& a, T b);
 template <class T> static inline bool chmin(T& a, T b);
 template <class A, size_t N, class T> static void Fill(A (&arr)[N], const T& val);
 
-vector<i_i> edges[1005][1005];
-int memo[1005][1005];
-bool visited[1005][1005];
+template <class T> struct Edge {
+  int src, to;
+  T cost;
 
-int dfs(int i, int j) {
-  if (memo[i][j] >= 0) return memo[i][j];
-  visited[i][j] = true;
+  Edge(int to, T cost) : src(-1), to(to), cost(cost) {}
+  Edge(int src, int to, T cost) : src(src), to(to), cost(cost) {}
 
-  int ret = 1;
-  for (i_i p : edges[i][j]) {
-    if (visited[p.first][p.second] && memo[p.first][p.second] < 0) return memo[i][j] = I_INF;
-    chmax(ret, dfs(p.first, p.second) + 1);
+  Edge& operator=(const int& x) {
+    to = x;
+    return *this;
   }
 
-  return memo[i][j] = ret;
+  operator int() const { return to; }
+};
+
+template <class T> using Edges = vector<Edge<T>>;
+template <class T> using WeightedGraph = vector<Edges<T>>;
+using UnWeightedGraph = vector<vector<int>>;
+template <class T> using Matrix = vector<vector<T>>;
+
+// @return -> not DAG if empty vector
+int topological_sort(vec<vec<vec<i_i>>>& adj_list) {
+  int v = sz(adj_list);
+  int ans = 0;
+  vec<vec<ll>> cnt(v, vec<ll>(v, 0));
+  stack<i_i> st;
+  rep(i, 0, v) rep(j, 0, v) {
+    for (i_i& p : adj_list[i][j]) {
+      cnt[p.fi][p.se]++;
+    }
+  }
+
+  rep(i, 0, v) rep(j, 0, v) if (cnt[i][j] == 0) st.push({i, j});
+
+  bool c = true;
+  while (c) {
+    vec<i_i> next_list;
+    while (st.size()) {
+      i_i cur = st.top();
+      st.pop();
+
+      for (auto to : adj_list[cur.fi][cur.se]) {
+        cnt[to.fi][to.se]--;
+        if (cnt[to.fi][to.se] == 0LL) next_list.pb(to);
+      }
+    }
+
+    ans++;
+    for (i_i p : next_list) st.push(p);
+    if (sz(next_list) == 0) c = false;
+  }
+
+  rep(i, 0, v) rep(j, 0, v) if (cnt[i][j] > 0) return -1;
+  return ans;
 }
 
 int main(int argc, char* argv[]) {
   int n;
   cin >> n;
-  rep(i, 0, n) {
-    i_i prev{-1, -1};
-    rep(j, 0, n - 1) {
-      int to;
-      cin >> to;
-      to--;
 
-      if (prev.first != -1) {
-        edges[prev.first][prev.second].pb({min(i, to), max(i, to)});
-      }
-      prev.first = min(i, to);
-      prev.second = max(i, to);
+  vec<vec<int>> match(n, vec<int>(n - 1));
+  vec<vec<vec<i_i>>> adj_list(n, vec<vec<i_i>>(n, vec<i_i>()));
+  rep(i, 0, n) scan(match[i]);
+
+  rep(i, 0, n) {
+    rep(j, 0, n - 2) {
+      int l = min(i + 1, match[i][j]), l2 = min(i + 1, match[i][j + 1]);
+      int r = max(i + 1, match[i][j]), r2 = max(i + 1, match[i][j + 1]);
+      l--, l2--, r--, r2--;
+      adj_list[l][r].emplace_back(l2, r2);
     }
   }
 
-  int ans = 0;
-  Fill(memo, int(-1));
-  rep(i, 0, n) rep(j, 0, n) {
-    if (i >= j) continue;
-    chmax(ans, dfs(i, j));
-  }
-
-  cout << (ans >= I_INF ? -1 : ans) << endl;
+  ll ans = topological_sort(adj_list);
+  cout << ans << endl;
   return 0;
 }
 
