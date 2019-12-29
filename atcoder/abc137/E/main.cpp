237737edf22199a6d17aadb8e6b4eee1cb25a14b
yukirin <standupdown@gmail.com>
2019-12-29 23:41:43 +0900

    Update abc137: E

--- atcoder/abc137/E/main.cpp
+++ atcoder/abc137/E/main.cpp
@@ -69,32 +69,7 @@ template <class T> using WeightedGraph = vector<Edges<T>>;
 using UnWeightedGraph = vector<vector<int>>;
 template <class T> using Matrix = vector<vector<T>>;
 
-// dijkstra (route restoration)
-template <class T> vector<T> dijkstra(int s, WeightedGraph<T>& adj_list, T inf) {
-  int si = sz(adj_list);
-  vector<T> dist(si, inf);
-
-  using Pi = pair<T, int>;
-  priority_queue<Pi, vector<Pi>, greater<Pi>> que;
-  dist[s] = 0;
-  que.emplace(dist[s], s);
-  while (!que.empty()) {
-    T cost;
-    int idx;
-    tie(cost, idx) = que.top();
-    que.pop();
-    if (dist[idx] < cost) continue;
-    for (auto& e : adj_list[idx]) {
-      auto next_cost = cost + e.cost;
-      if (dist[e.to] <= next_cost) continue;
-      dist[e.to] = next_cost;
-      que.emplace(dist[e.to], e.to);
-    }
-  }
-  return dist;
-}
-
-template <class T> vector<T> bellman_ford(Edges<T>& edges, int V, int s, T inf, WeightedGraph<T>& adj_list) {
+template <class T> vector<T> bellman_ford(Edges<T>& edges, int V, int s, T inf) {
   vector<T> dist(V, inf);
   dist[s] = 0;
   for (int i = 0; i < V - 1; i++) {
@@ -104,18 +79,22 @@ template <class T> vector<T> bellman_ford(Edges<T>& edges, int V, int s, T inf, 
     }
   }
 
-  vector<T> before(dist);
-
   for (auto& e : edges) {
     if (dist[e.src] == inf) continue;
     if (dist[e.src] + e.cost < dist[e.to]) {
-      auto v = dijkstra(e.to, adj_list, inf);
-      if (v[V - 1] == LL_INF) continue;
-      return vector<T>();
+      dist[e.to] = -inf;
     }
   }
 
-  if (dist[V - 1] < before[V - 1]) return vec<T>();
+  for (int i = 0; i < V; i++) {
+    for (auto& e : edges) {
+      if (dist[e.src] == inf) continue;
+      dist[e.to] = min(dist[e.to], dist[e.src] + e.cost);
+      if (dist[e.src] == -inf) dist[e.to] = -inf;
+    }
+  }
+
+  if (dist[V - 1] <= -inf) return vec<T>();
   return dist;
 }
 
@@ -126,16 +105,14 @@ int main(int argc, char* argv[]) {
   for (int i = 0; i < m; i++) scanf("%lld %lld %lld", &a[i], &b[i], &c[i]);
 
   Edges<ll> es(m, Edge<ll>());
-  WeightedGraph<ll> adj_list(n, Edges<ll>());
 
   rep(i, 0, m) {
     es[i].src = a[i] - 1;
     es[i].to = b[i] - 1;
     es[i].cost = -(c[i] - p);
-    adj_list[a[i] - 1].emplace_back(b[i] - 1, 1);
   }
 
-  auto v = bellman_ford(es, n, 0, LL_INF, adj_list);
+  auto v = bellman_ford(es, n, 0, LL_INF);
   if (v.empty()) {
     put(-1);
     return 0;
