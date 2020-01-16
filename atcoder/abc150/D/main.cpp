f00050118f0b4a424eb06af8d88852e535e65451
yukirin <standupdown@gmail.com>
2020-01-17 04:12:55 +0900

    Add abc150

---
+++ atcoder/abc150/D/main.cpp
@@ -0,0 +1,159 @@
+#include <bits/stdc++.h>
+
+#ifdef _DEBUG
+#define debug(x) cerr << "line: " << __LINE__ << ", func: " << __func__ << " ->  " << #x << " = " << x << endl
+#else
+#define debug(x)
+#endif
+
+#define all(s) begin(s), end(s)
+#define rall(s) rbegin(s), rend(s)
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = ((a)-1); i >= (b); i--)
+#define pb push_back
+#define sz(a) int((a).size())
+#define put(a) ((cout) << (a) << (endl))
+#define putf(a, n) ((cout) << (fixed) << (setprecision(n)) << (a) << (endl))
+#define deg2rad(x) (((x)*PI) / (180.0))
+#define rad2deg(x) (((x) * (180.0)) / PI)
+#define fi first
+#define se second
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using i_i = pair<int, int>;
+using ll_ll = pair<ll, ll>;
+using d_ll = pair<double, ll>;
+using ll_d = pair<ll, double>;
+using d_d = pair<double, double>;
+template <class T> using vec = vector<T>;
+
+static constexpr ll LL_INF = 1LL << 60;
+static constexpr int I_INF = 1 << 28;
+static constexpr double PI = static_cast<double>(3.14159265358979323846264338327950288);
+static constexpr double EPS = numeric_limits<double>::epsilon();
+
+static map<type_index, const char* const> scanType = {
+    {typeid(int), "%d"}, {typeid(ll), "%lld"}, {typeid(double), "%lf"}, {typeid(char), "%c"}};
+
+template <class T> static void scan(vector<T>& v);
+[[maybe_unused]] static void scan(vector<string>& v, bool isWord = true);
+template <class T> static inline bool chmax(T& a, T b);
+template <class T> static inline bool chmin(T& a, T b);
+template <class T> static inline T gcd(T a, T b);
+template <class T> static inline T lcm(T a, T b);
+template <class A, size_t N, class T> static void Fill(A (&arr)[N], const T& val);
+template <class T> T mod(T a, T m);
+
+// 拡張 Euclid の互除法
+// ap + bq = gcd(a, b) となる (p, q) を求め、d = gcd(a, b) をリターンします
+long long extGcd(long long a, long long b, long long& p, long long& q) {
+  if (b == 0) {
+    p = 1, q = 0;
+    return a;
+  }
+  long long d = extGcd(b, a % b, q, p);
+  q -= a / b * p;
+  return d;
+}
+
+// 中国剰余定理
+// リターン値を (r, m) とすると解は x ≡ r (mod. m)
+// 解なしの場合は (0, -1) をリターン
+pair<long long, long long> ChineseRem(const vector<long long>& b, const vector<long long>& m) {
+  long long r = 0, M = 1;
+  for (int i = 0; i < (int)b.size(); ++i) {
+    long long p, q;
+    long long d = extGcd(M, m[i], p, q);  // p is inv of M/d (mod. m[i]/d)
+    if ((b[i] - r) % d != 0) return make_pair(0, -1);
+    long long tmp = (b[i] - r) / d * p % (m[i] / d);
+    r += M * tmp;
+    M *= m[i] / d;
+  }
+  return make_pair(mod(r, M), M);
+}
+
+int main(int argc, char* argv[]) {
+  ll n, m;
+  cin >> n >> m;
+  vec<ll> a(n);
+  scan(a);
+
+  vec<ll> b(n), mo(n);
+  rep(i, 0, n) {
+    b[i] = a[i] / 2;
+    mo[i] = a[i];
+  }
+
+  auto p = ChineseRem(b, mo);
+  if (p.fi > m || p.se == -1) {
+    put(0);
+    return 0;
+  }
+
+  m -= p.fi;
+  ll ans = m / p.se + 1;
+  put(ans);
+  return 0;
+}
+
+template <class T> static void scan(vector<T>& v) {
+  auto tFormat = scanType[typeid(T)];
+  for (T& n : v) {
+    scanf(tFormat, &n);
+  }
+}
+
+static void scan(vector<string>& v, bool isWord) {
+  if (isWord) {
+    for (auto& n : v) {
+      cin >> n;
+    }
+    return;
+  }
+
+  int i = 0, size = v.size();
+  string s;
+  getline(cin, s);
+
+  if (s.size() != 0) {
+    i++;
+    v[0] = s;
+  }
+
+  for (; i < size; ++i) {
+    getline(cin, v[i]);
+  }
+}
+
+template <class T> inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <class T> inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <class T> inline T gcd(T a, T b) { return __gcd(a, b); }
+
+template <class T> inline T lcm(T a, T b) {
+  T c = min(a, b), d = max(a, b);
+  return c * (d / gcd(c, d));
+}
+
+template <class A, size_t N, class T> void Fill(A (&arr)[N], const T& val) {
+  std::fill((T*)arr, (T*)(arr + N), val);
+}
+
+template <class T> T mod(T a, T m) { return (a % m + m) % m; }
