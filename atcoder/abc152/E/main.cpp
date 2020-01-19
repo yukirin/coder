d75f50490a8f7df87b3c2c72aa88d4d778368e4e
yukirin <standupdown@gmail.com>
2020-01-20 02:06:33 +0900

    Add abc152

---
+++ atcoder/abc152/E/main.cpp
@@ -0,0 +1,185 @@
+#include <bits/stdc++.h>
+
+#ifdef _DEBUG
+#define debug(x) cerr << "line: " << __LINE__ << ", func: " << __func__ << " ->  " << #x << " = " << x << endl
+#else
+#define debug(x)
+#endif
+
+#define all(s) begin(s), end(s)
+#define rall(s) rbegin(s), rend(s)
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = ((a)-1); i >= (b); i--)
+#define pb push_back
+#define sz(a) int((a).size())
+#define put(a) ((cout) << (a) << (endl))
+#define putf(a, n) ((cout) << (fixed) << (setprecision(n)) << (a) << (endl))
+#define deg2rad(x) (((x)*PI) / (180.0))
+#define rad2deg(x) (((x) * (180.0)) / PI)
+#define fi first
+#define se second
+#define F first
+#define S second
+#define MAX 10000003
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using i_i = pair<int, int>;
+using ll_ll = pair<ll, ll>;
+using d_ll = pair<double, ll>;
+using ll_d = pair<ll, double>;
+using d_d = pair<double, double>;
+template <class T> using vec = vector<T>;
+
+static constexpr ll LL_INF = 1LL << 60;
+static constexpr int I_INF = 1 << 28;
+static constexpr double PI = static_cast<double>(3.14159265358979323846264338327950288);
+static constexpr double EPS = numeric_limits<double>::epsilon();
+static constexpr ll MOD = 1000000007;
+
+static map<type_index, const char* const> scanType = {
+    {typeid(int), "%d"}, {typeid(ll), "%lld"}, {typeid(double), "%lf"}, {typeid(char), "%c"}};
+
+template <class T> static void scan(vector<T>& v);
+[[maybe_unused]] static void scan(vector<string>& v, bool isWord = true);
+template <class T> static inline bool chmax(T& a, T b);
+template <class T> static inline bool chmin(T& a, T b);
+template <class T> static inline T gcd(T a, T b);
+template <class T> static inline T lcm(T a, T b);
+template <class A, size_t N, class T> static void Fill(A (&arr)[N], const T& val);
+template <class T> T mod(T a, T m);
+
+ll modpow(ll a, ll n, ll p) {
+  ll res = 1;
+  while (n > 0) {
+    if (n & 1) res = res * a % p;
+    a = a * a % p;
+    n >>= 1;
+  }
+  return res;
+}
+
+vector<ll_ll> prime_factor(ll n) {
+  vector<ll_ll> res;
+  for (ll i = 2; i * i <= n; i++) {
+    if (n % i) continue;
+    res.emplace_back(i, 0);
+    while (n % i == 0) {
+      n /= i;
+      res.back().second++;
+    }
+  }
+
+  if (n != 1) res.emplace_back(n, 1);
+  return res;
+}
+
+ll modinv(ll a, ll p) {
+  ll b = p, u = 1, v = 0;
+  while (b) {
+    ll t = a / b;
+    a -= t * b;
+    swap(a, b);
+    u -= t * v;
+    swap(u, v);
+  }
+
+  u %= p;
+  if (u < 0) u += p;
+  return u;
+}
+
+ll modlcm(vector<ll>& ar, ll p) {
+  unordered_map<ll, ll> m;
+  for (ll num : ar) {
+    auto v = prime_factor(num);
+    for (ll_ll pa : v) chmax(m[pa.fi], pa.se);
+  }
+
+  ll ret = 1;
+  for (auto pa : m) {
+    ret *= modpow(pa.fi, pa.se, p);
+    ret %= p;
+  }
+
+  return ret;
+}
+
+int main(int argc, char* argv[]) {
+  ll n;
+  cin >> n;
+  vec<ll> a(n);
+  scan(a);
+
+  ll ans = 0;
+  ll l = modlcm(a, MOD);
+
+  rep(i, 0, n) {
+    ans += mod(l * modinv(a[i], MOD), MOD);
+    ans %= MOD;
+  }
+
+  put(ans);
+  return 0;
+}
+
+template <class T> static void scan(vector<T>& v) {
+  auto tFormat = scanType[typeid(T)];
+  for (T& n : v) {
+    scanf(tFormat, &n);
+  }
+}
+
+static void scan(vector<string>& v, bool isWord) {
+  if (isWord) {
+    for (auto& n : v) {
+      cin >> n;
+    }
+    return;
+  }
+
+  int i = 0, size = v.size();
+  string s;
+  getline(cin, s);
+
+  if (s.size() != 0) {
+    i++;
+    v[0] = s;
+  }
+
+  for (; i < size; ++i) {
+    getline(cin, v[i]);
+  }
+}
+
+template <class T> inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <class T> inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+
+template <class T> inline T gcd(T a, T b) { return __gcd(a, b); }
+
+template <class T> inline T lcm(T a, T b) {
+  T c = min(a, b), d = max(a, b);
+  return c * (d / gcd(c, d));
+}
+
+template <class A, size_t N, class T> void Fill(A (&arr)[N], const T& val) {
+  std::fill((T*)arr, (T*)(arr + N), val);
+}
+
+template <class T> T mod(T a, T m) { return (a % m + m) % m; }
