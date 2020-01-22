224b4ced2ae831fa8496fad093de96c5e2397cf6
yukirin <standupdown@gmail.com>
2020-01-23 02:28:02 +0900

    Update abc127 E

--- atcoder/abc127/E/main.cpp
+++ atcoder/abc127/E/main.cpp
@@ -9,10 +9,12 @@
 #define all(s) begin(s), end(s)
 #define rall(s) rbegin(s), rend(s)
 #define rep(i, a, b) for (int i = (a); i < (b); i++)
-#define rrep(i, a, b) for (int i = ((a) - 1); i >= (b); i--)
+#define rrep(i, a, b) for (int i = ((a)-1); i >= (b); i--)
 #define pb push_back
 #define sz(a) int((a).size())
 #define put(a) ((cout) << (a) << (endl))
+#define fi first
+#define se second
 
 using namespace std;
 
@@ -23,6 +25,7 @@ using ll_ll = pair<ll, ll>;
 using d_ll = pair<double, ll>;
 using ll_d = pair<ll, double>;
 using d_d = pair<double, double>;
+template <class T> using vec = vector<T>;
 
 static constexpr ll LL_INF = 1LL << 60;
 static constexpr int I_INF = 1 << 28;
@@ -41,14 +44,50 @@ template <class T> static inline T gcd(T a, T b);
 template <class T> static inline T lcm(T a, T b);
 template <class A, size_t N, class T> static void Fill(A (&arr)[N], const T& val);
 
-int main(int argc, char* argv[]) {
-  long long N;
-  scanf("%lld",&N);
-  long long M;
-  scanf("%lld",&M);
-  long long K;
-  scanf("%lld",&K);
+static constexpr int MAX = 210000;
+ll fac[MAX], finv[MAX], inv[MAX];
 
+// preprocess
+void comb_init() {
+  fac[0] = fac[1] = 1;
+  finv[0] = finv[1] = 1;
+  inv[1] = 1;
+  for (int i = 2; i < MAX; i++) {
+    fac[i] = fac[i - 1] * i % MOD;
+    inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+    finv[i] = finv[i - 1] * inv[i] % MOD;
+  }
+}
+
+// nCr mod p
+ll comb(int n, int k) {
+  if (n < k) return 0;
+  if (n < 0 || k < 0) return 0;
+  return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+ll mod(ll num) { return num % MOD; }
+
+int main(int argc, char* argv[]) {
+  ll n, m, k;
+  cin >> n >> m >> k;
+  comb_init();
+
+  ll ans = 0;
+  ll c_cnt = comb(n * m - 2, k - 2);
+  rep(d, 1, n) {
+    ll cnt = mod(mod(c_cnt * (n - d)) * mod(m * m));
+    ans += mod(d * cnt);
+    ans %= MOD;
+  }
+
+  rep(d, 1, m) {
+    ll cnt = mod(mod(c_cnt * (m - d)) * mod(n * n));
+    ans += mod(d * cnt);
+    ans %= MOD;
+  }
+
+  put(ans);
   return 0;
 }
 
@@ -97,13 +136,9 @@ template <class T> inline bool chmin(T& a, T b) {
   return 0;
 }
 
-template <class T> inline T gcd(T a, T b) {
-  return __gcd(a, b);
-}
+template <class T> inline T gcd(T a, T b) { return __gcd(a, b); }
 
-template <class T> inline T lcm(T a, T b) {
-  return (a * b) / gcd(a, b);
-}
+template <class T> inline T lcm(T a, T b) { return (a * b) / gcd(a, b); }
 
 template <class A, size_t N, class T> void Fill(A (&arr)[N], const T& val) {
   std::fill((T*)arr, (T*)(arr + N), val);
