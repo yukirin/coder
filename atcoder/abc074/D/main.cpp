6499bce219f6299efe63e8aeb50cf77dbd3fbcb9
yukirin <standupdown@gmail.com>
2020-01-25 02:17:20 +0900

    Update 074 D

--- atcoder/abc074/D/main.cpp
+++ atcoder/abc074/D/main.cpp
@@ -9,13 +9,15 @@
 #define all(s) begin(s), end(s)
 #define rall(s) rbegin(s), rend(s)
 #define rep(i, a, b) for (int i = (a); i < (b); i++)
-#define rrep(i, a, b) for (int i = ((a) - 1); i >= (b); i--)
+#define rrep(i, a, b) for (int i = ((a)-1); i >= (b); i--)
 #define pb push_back
 #define sz(a) int((a).size())
 #define put(a) ((cout) << (a) << (endl))
 #define putf(a, n) ((cout) << (fixed) << (setprecision(n)) << (a) << (endl))
-#define deg2rad(x) (((x) * PI) / (180.0))
+#define deg2rad(x) (((x)*PI) / (180.0))
 #define rad2deg(x) (((x) * (180.0)) / PI)
+#define fi first
+#define se second
 
 using namespace std;
 
@@ -45,16 +47,80 @@ template <class T> static inline T lcm(T a, T b);
 template <class A, size_t N, class T> static void Fill(A (&arr)[N], const T& val);
 template <class T> T mod(T a, T m);
 
-int main(int argc, char* argv[]) {
-  long long N;
-  scanf("%lld",&N);
-  std::vector<std::vector<long long>> A(N, std::vector<long long>(N));
-  for(int i = 0 ; i < N ; i++){
-    for(int j = 0 ; j < N ; j++){
-      scanf("%lld",&A[i][j]);
-    }
+template <class T> struct Edge {
+  int src, to;
+  T cost;
+
+  Edge(int to, T cost) : src(-1), to(to), cost(cost) {}
+  Edge(int src, int to, T cost) : src(src), to(to), cost(cost) {}
+
+  Edge& operator=(const int& x) {
+    to = x;
+    return *this;
   }
 
+  operator int() const { return to; }
+};
+
+template <class T> using Edges = vector<Edge<T>>;
+template <class T> using WeightedGraph = vector<Edges<T>>;
+using UnWeightedGraph = vector<vector<int>>;
+template <class T> using Matrix = vector<vector<T>>;
+
+// dijkstra (route restoration)
+// ! init process -> prev[i] = -1
+template <class T> vector<T> dijkstra(int s, WeightedGraph<T>& adj_list, T inf) {
+  int si = sz(adj_list);
+  vector<T> dist(si, inf);
+
+  using Pi = pair<T, int>;
+  priority_queue<Pi, vector<Pi>, greater<Pi>> que;
+  dist[s] = 0;
+  que.emplace(dist[s], s);
+  while (!que.empty()) {
+    T cost;
+    int idx;
+    tie(cost, idx) = que.top();
+    que.pop();
+    if (dist[idx] < cost) continue;
+    for (auto& e : adj_list[idx]) {
+      auto next_cost = cost + e.cost;
+      if (dist[e.to] <= next_cost) continue;
+      dist[e.to] = next_cost;
+      que.emplace(dist[e.to], e.to);
+    }
+  }
+  return dist;
+}
+
+int main(int argc, char* argv[]) {
+  ll n;
+  cin >> n;
+  vec<vec<ll>> a(n, vec<ll>(n));
+  rep(i, 0, n) rep(j, 0, n) scanf("%lld", &a[i][j]);
+
+  ll ans = 0;
+  vec<pair<ll, ll_ll>> edges;
+  rep(i, 0, n) rep(j, i + 1, n) edges.emplace_back(a[i][j], make_pair(i, j));
+  WeightedGraph<ll> adj(n);
+  sort(all(edges));
+
+  rep(i, 0, sz(edges)) {
+    ll cost = edges[i].fi;
+    ll_ll edge = edges[i].se;
+
+    auto paths = dijkstra(edge.fi, adj, LL_INF);
+    if (paths[edge.se] < cost) {
+      cout << -1 << endl;
+      return 0;
+    }
+    if (paths[edge.se] == cost) continue;
+    adj[edge.fi].emplace_back(edge.se, cost);
+    adj[edge.se].emplace_back(edge.fi, cost);
+    ans += cost;
+  }
+
+  cout << ans << endl;
   return 0;
 }
 
@@ -103,18 +169,12 @@ template <class T> inline bool chmin(T& a, T b) {
   return 0;
 }
 
-template <class T> inline T gcd(T a, T b) {
-  return __gcd(a, b);
-}
+template <class T> inline T gcd(T a, T b) { return __gcd(a, b); }
 
-template <class T> inline T lcm(T a, T b) {
-  return (a * b) / gcd(a, b);
-}
+template <class T> inline T lcm(T a, T b) { return (a * b) / gcd(a, b); }
 
 template <class A, size_t N, class T> void Fill(A (&arr)[N], const T& val) {
   std::fill((T*)arr, (T*)(arr + N), val);
 }
 
-template <class T> T mod(T a, T m) {
-  return (a % m + m) % m;
-}
+template <class T> T mod(T a, T m) { return (a % m + m) % m; }
