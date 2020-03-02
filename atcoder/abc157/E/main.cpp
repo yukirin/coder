5c30fdfb3ea4950692d19e249a1260ea5ffdb57a
yukirin <standupdown@gmail.com>
2020-03-02 16:03:22 +0900

    Add abc157: E

--- atcoder/abc157/E/main.cpp
+++ atcoder/abc157/E/main.cpp
@@ -9,12 +9,12 @@
 #define all(s) begin(s), end(s)
 #define rall(s) rbegin(s), rend(s)
 #define rep(i, a, b) for (int i = (a); i < (b); i++)
-#define rrep(i, a, b) for (int i = ((a) - 1); i >= (b); i--)
+#define rrep(i, a, b) for (int i = ((a)-1); i >= (b); i--)
 #define pb push_back
 #define sz(a) int((a).size())
 #define put(a) ((cout) << (a) << (endl))
 #define putf(a, n) ((cout) << (fixed) << (setprecision(n)) << (a) << (endl))
-#define deg2rad(x) (((x) * PI) / (180.0))
+#define deg2rad(x) (((x)*PI) / (180.0))
 #define rad2deg(x) (((x) * (180.0)) / PI)
 #define fi first
 #define se second
@@ -48,8 +48,77 @@ template <class T> static inline T lcm(T a, T b);
 template <class A, size_t N, class T> static void Fill(A (&arr)[N], const T& val);
 template <class T> T mod(T a, T m);
 
-int main(int argc, char* argv[]) {
+template <typename Monoid> struct SegmentTree {
+  using F = function<Monoid(Monoid, Monoid)>;
 
+  int sz;
+  vector<Monoid> seg;
+
+  const F f;
+  const Monoid M1;
+
+  SegmentTree(int n, const F f, const Monoid& M1) : f(f), M1(M1) {
+    sz = 1;
+    while (sz < n) sz <<= 1;
+    seg.assign(2 * sz, M1);
+  }
+
+  void set(int k, const Monoid& x) { seg[k + sz] = x; }
+
+  void build() {
+    for (int k = sz - 1; k > 0; k--) {
+      seg[k] = f(seg[2 * k + 0], seg[2 * k + 1]);
+    }
+  }
+
+  void update(int k, const Monoid& x) {
+    k += sz;
+    seg[k] = x;
+    while (k >>= 1) {
+      seg[k] = f(seg[2 * k + 0], seg[2 * k + 1]);
+    }
+  }
+
+  Monoid query(int a, int b) {
+    Monoid L = M1, R = M1;
+    for (a += sz, b += sz; a < b; a >>= 1, b >>= 1) {
+      if (a & 1) L = f(L, seg[a++]);
+      if (b & 1) R = f(seg[--b], R);
+    }
+    return f(L, R);
+  }
+
+  Monoid operator[](const int& k) const { return seg[k + sz]; }
+};
+
+int main(int argc, char* argv[]) {
+  ll n, q;
+  string s;
+  cin >> n >> s >> q;
+
+  auto f = [](int a, int b) { return a | b; };
+  SegmentTree<int> seg(n, f, 0);
+  rep(i, 0, n) { seg.set(i, 1 << (s[i] - 'a')); }
+  seg.build();
+
+  string ans = "";
+  rep(i, 0, q) {
+    int type;
+    cin >> type;
+    if (type == 1) {
+      int a;
+      char c;
+      cin >> a >> c;
+      seg.update(a - 1, 1 << (c - 'a'));
+      continue;
+    }
+
+    int a, b;
+    cin >> a >> b;
+    ans += to_string(__builtin_popcount(seg.query(a - 1, b))) + "\n";
+  }
+
+  cout << ans;
   return 0;
 }
 
@@ -98,9 +167,7 @@ template <class T> inline bool chmin(T& a, T b) {
   return 0;
 }
 
-template <class T> inline T gcd(T a, T b) {
-  return __gcd(a, b);
-}
+template <class T> inline T gcd(T a, T b) { return __gcd(a, b); }
 
 template <class T> inline T lcm(T a, T b) {
   T c = min(a, b), d = max(a, b);
@@ -111,6 +178,4 @@ template <class A, size_t N, class T> void Fill(A (&arr)[N], const T& val) {
   std::fill((T*)arr, (T*)(arr + N), val);
 }
 
-template <class T> T mod(T a, T m) {
-  return (a % m + m) % m;
-}
+template <class T> T mod(T a, T m) { return (a % m + m) % m; }
