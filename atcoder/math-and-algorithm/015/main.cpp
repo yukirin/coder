be5986f0af0dee881529ac8579fd168e6c3f879d
yukirin <standupdown@gmail.com>
2022-08-20 03:23:32 +0900

    add math-and-algo 015

--- atcoder/math-and-algorithm/015/main.cpp
+++ atcoder/math-and-algorithm/015/main.cpp
@@ -37,7 +37,6 @@ static constexpr int I_INF = 1 << 28;
 static constexpr double PI = static_cast<double>(3.14159265358979323846264338327950288);
 static constexpr double EPS = numeric_limits<double>::epsilon();
 
-
 static map<type_index, const char* const> scanType = {
     {typeid(int), "%d"}, {typeid(ll), "%lld"}, {typeid(double), "%lf"}, {typeid(char), "%c"}};
 
@@ -54,6 +53,7 @@ int main(int argc, char* argv[]) {
   long long B;
   std::scanf("%lld", &B);
 
+  put(gcd(A, B));
   return 0;
 }
 
