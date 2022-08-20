dd3a1fce40bfe63152ad211653aef3fdcf488b43
yukirin <standupdown@gmail.com>
2022-08-21 00:49:33 +0900

    add math-and-algo 023

--- atcoder/math-and-algorithm/023/main.cpp
+++ atcoder/math-and-algorithm/023/main.cpp
@@ -37,7 +37,6 @@ static constexpr int I_INF = 1 << 28;
 static constexpr double PI = static_cast<double>(3.14159265358979323846264338327950288);
 static constexpr double EPS = numeric_limits<double>::epsilon();
 
-
 static map<type_index, const char* const> scanType = {
     {typeid(int), "%d"}, {typeid(ll), "%lld"}, {typeid(double), "%lf"}, {typeid(char), "%c"}};
 
@@ -52,14 +51,18 @@ int main(int argc, char* argv[]) {
   long long N;
   std::scanf("%lld", &N);
   std::vector<long long> B(N);
-  for(int i = 0 ; i < N ; i++){
+  for (int i = 0; i < N; i++) {
     std::scanf("%lld", &B[i]);
   }
   std::vector<long long> R(N);
-  for(int i = 0 ; i < N ; i++){
+  for (int i = 0; i < N; i++) {
     std::scanf("%lld", &R[i]);
   }
 
+  ll b_total = accumulate(B.begin(), B.end(), 0LL);
+  ll r_total = accumulate(R.begin(), R.end(), 0LL);
+
+  putf(b_total / ld(N) + r_total / ld(N), 7);
   return 0;
 }
 
