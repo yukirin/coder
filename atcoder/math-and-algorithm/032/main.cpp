3a04e9d19011b126bf4b3b0e6d089e6746fb58d5
yukirin <standupdown@gmail.com>
2022-09-19 23:10:37 +0900

    add math-and-algo 032

--- atcoder/math-and-algorithm/032/main.cpp
+++ atcoder/math-and-algorithm/032/main.cpp
@@ -37,8 +37,8 @@ static constexpr int I_INF = 1 << 28;
 static constexpr double PI = static_cast<double>(3.14159265358979323846264338327950288);
 static constexpr double EPS = numeric_limits<double>::epsilon();
 
-static constexpr YES = "Yes";
-static constexpr NO = "No";
+static constexpr const char* YES = "Yes";
+static constexpr const char* NO = "No";
 
 static map<type_index, const char* const> scanType = {
     {typeid(int), "%d"}, {typeid(ll), "%lld"}, {typeid(double), "%lf"}, {typeid(char), "%c"}};
@@ -56,9 +56,13 @@ int main(int argc, char* argv[]) {
   long long X;
   std::scanf("%lld", &X);
   std::vector<long long> A(N);
-  for(int i = 0 ; i < N ; i++){
+  for (int i = 0; i < N; i++) {
     std::scanf("%lld", &A[i]);
   }
+
+  sort(A.begin(), A.end());
+  const auto isExist = binary_search(A.begin(), A.end(), X);
+  put(isExist ? YES : NO);
 
   return 0;
 }
